9a37726aa6101605d21740464b254a010091a40b
Abhishek Lale <abhisheklale181@gmail.com>
2021-04-11 15:56:46 +0530

    add about text

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.get('', (req,res)=> {
 })
 app.get('/about', (req,res)=> {
     res.render('about',{
-        title: 'About us',
+        title: 'About me',
         name: 'Abhishek'
     })
 })
