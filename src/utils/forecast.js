5466f4c4adf2fded2c5e29653cb2c55251c19d62
Abhishek Lale <abhisheklale181@gmail.com>
2021-04-11 16:13:54 +0530

    changed forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude,longitude,callback) =>{
             callback('unable to find location',undefined)
         }
         else{
-            callback(undefined, 'it is currently '+body.current.temperature+' degrees out.')
+            callback(undefined, 'it is currently '+body.current.temperature+' degrees out. The humidity is '+body.current.humidity + '%')
         }
     })
 }
